96e89b1580f8089ab600bde5b68e1935d55ed5c1
amwaj <MOOJY557@GMAIL.COM>
2026-04-04 08:45:12 +0300

    sec editing

--- Ha-Ossooll.Data/Configurations/DataConfiguration.cs
+++ Ha-Ossooll.Data/Configurations/DataConfiguration.cs
@@ -2,7 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using HA_Ossooll.Data.Data;
 
 namespace HA_Ossooll.Data.Configurations;
 
@@ -11,10 +10,11 @@ public static class DataConfiguration
     public static IServiceCollection AddProjectDataLayer(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
-
-        //services.AddDbContext<AppDbContext>(options =>
-        //    options.UseSqlServer(AppDbContext.DBConnectionString));
+            options.UseMySql(
+                configuration.GetConnectionString("DefaultConnection"),
+                ServerVersion.AutoDetect(configuration.GetConnectionString("DefaultConnection"))
+            )
+        );
 
         return services;
     }
